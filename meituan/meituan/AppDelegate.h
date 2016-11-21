7699606cdfd7c85d6313fdf679dcc8fc5919fc03
apple009 <20777159@qq.com>
2016-11-21 15:06:22 +0800

    test iOS in git

---
+++ meituan/meituan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  meituan
+//
+//  Created by Apple009 on 2016/11/21.
+//  Copyright © 2016年 jiah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ meituan/meituan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  meituan
+//
+//  Created by Apple009 on 2016/11/21.
+//  Copyright © 2016年 jiah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
